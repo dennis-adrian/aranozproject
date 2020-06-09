5420a2363790c8f4e07de73ec578a65b2dc84025
Dennis Adrian <adriangbetancur@gmail.com>
2020-06-08 22:26:42 -0400

    Limpieza de páginas y reciclado de algunas secciones de código

--- header.php
+++ header.php
@@ -21,8 +21,8 @@
                                 </a>
                                 <div class="dropdown-menu" aria-labelledby="navbarDropdown_1">
                                     <a class="dropdown-item" href="category.php">
-                                        shop category</a>
-                                    <a class="dropdown-item" href="single-product.php">product details</a>
+                                        products</a>
+                                    <a class="dropdown-item" href="cart.php?op=comprar">shopping cart</a>
                                 </div>
                             </li>
                             <li class="nav-item dropdown">
@@ -31,20 +31,7 @@
                                 </a>
                                 <div class="dropdown-menu" aria-labelledby="navbarDropdown_2">
                                     <a class="dropdown-item" href="login.php"> login</a>
-                                    <a class="dropdown-item" href="tracking.php">tracking</a>
-                                    <a class="dropdown-item" href="checkout.php">product checkout</a>
-                                    <a class="dropdown-item" href="cart.php">shopping cart</a>
-                                    <a class="dropdown-item" href="confirmation.php">confirmation</a>
-                                    <a class="dropdown-item" href="elements.php">elements</a>
-                                </div>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="blog.php" id="navbarDropdown_2" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    blog
-                                </a>
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdown_2">
-                                    <a class="dropdown-item" href="blog.php"> blog</a>
-                                    <a class="dropdown-item" href="single-blog.php">Single blog</a>
+                                    <a class="dropdown-item" href="register.php">register</a>
                                 </div>
                             </li>
 
@@ -55,16 +42,16 @@
                     </div>
                     <div class="hearer_icon d-flex">
                         <a id="search_1" href="javascript:void(0)"><i class="ti-search"></i></a>
-                        <a href=""><i class="ti-heart"></i></a>
+                        <a href="cart.php?op=comprar"><i class="fas fa-cart-plus"></i></a>
                         <div class="dropdown cart">
-                            <a class="dropdown-toggle" href="cart.php" id="navbarDropdown3" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <a class="dropdown-toggle" href="" id="navbarDropdown3" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <i class="fas fa-cart-plus"></i>
                             </a>
-                            <!-- <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <div class="single_product">
-    
-                                    </div>
-                                </div> -->
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <div class="single_product">
+
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </nav>
